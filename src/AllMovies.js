dc52d1d9b3d0f30f22285ecc4ee054776f51d48c
Matt Saqueli <msaqueli22@turing.edu>
2023-05-12 18:07:11 -0500

    Display single movie

--- src/AllMovies.js
+++ src/AllMovies.js
@@ -1,32 +1,13 @@
-// import './AllMovies.css'
-// import React from 'react'
-
-// const AllMovies = (movies) => {
-//   return (
-//     movies.showMovies.map((movie) => {
-//       return (
-//         <img className='moviePoster'
-//           src={movie.poster_path} 
-//           key={movie.id}
-//           // onClick={() => singleMovie(movie.id)}
-//           />
-//       )
-//     })
-//   )
-// }
-
-// export default AllMovies
-
-
 import './AllMovies.css'
 import React from 'react'
 
-const AllMovies = ({showMovies}) => {
-  const movieCards = showMovies.map(movie => {
+const AllMovies = (props) => {
+  const movieCards = props.showMovies.map(movie => {
     return (
       <img className='movie-card'
         src={movie.poster_path} 
         key={movie.id}
+        onClick={() => props.displaySelectedMovie(movie.id)}
       />
     )
   })
