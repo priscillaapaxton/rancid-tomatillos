285867a9d8f90cc01ac00275ee190129eac3909a
Matt Saqueli <msaqueli22@turing.edu>
2023-05-19 20:43:42 -0500

    App cypress testing

---
+++ cypress/e2e/app_spec.cy.js
@@ -0,0 +1,34 @@
+describe('App', () => {
+  beforeEach(() => {
+    cy.intercept('GET', "https://rancid-tomatillos.herokuapp.com/api/v2/movies/", {
+      statusCode: 201,
+      fixture: "movies.json"
+    })
+    cy.visit('http://localhost:3000/')
+  })
+
+  it('As a user, when I load the application, I can see the RT logo', () => {
+    cy.get('.App header')
+      .find('a') 
+      .find('img.logo')
+      .invoke('attr', 'src')
+      .should('include', 'Rancid-tomatillos')
+  })
+
+  it('As a user, when I load the application, I can see all movies', () => {
+    cy.get('div')
+      .get('.movie-container')
+      .should('be.visible')
+  })
+
+  it('As a user, when I click on a movie, an individual movie page is displayed', () => {
+    // cy.fixture("singleMovie.json")
+    //   .then(movie => {
+    //   cy.intercept('GET',"https://rancid-tomatillos.herokuapp.com/api/v2/movies/436270", movie)
+    // });
+
+    // cy.get('.movie-container')
+    //   .get('.movie-card')
+      // .get('436270').click()
+  })
+})
